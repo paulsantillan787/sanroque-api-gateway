fc4e27030858e4786e22d287eb398a3c9658eba9
JoArDiTo <sm.armando05@gmail.com>
2025-05-05 20:24:56 -0500

    feat: remove UpdateTestPerformedDto as it is no longer needed

--- src/services/evaluation/test-performed/dto/update-test-performed.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestPerformedDto } from './create-test-performed.dto';
-
-export class UpdateTestPerformedDto extends PartialType(CreateTestPerformedDto) {}
