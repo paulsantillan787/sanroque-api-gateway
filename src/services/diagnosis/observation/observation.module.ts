0cb0af45337a4bcc84ad20b46492def8706d4bc8
Paul Santillan <110622811+paulsantillan787@users.noreply.github.com>
2026-07-15 15:16:45 -0500

    Refactor observation.module.ts to remove DIAGNOSIS_SERVICE
    Removed DIAGNOSIS_SERVICE from imports in the observation module.

--- src/services/diagnosis/observation/observation.module.ts
+++ src/services/diagnosis/observation/observation.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ObservationController } from './observation.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { AUTH_SERVICE, DIAGNOSIS_SERVICE, envs } from 'src/config';
+import { AUTH_SERVICE, envs } from 'src/config';
 
 @Module({
   controllers: [ObservationController],
   providers: [],
   imports: [
     ClientsModule.register([
-      {
-        name: DIAGNOSIS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: envs.diagnosisServiceHost,
-          port: envs.diagnosisServicePort
-        }
-      },
       {
         name: AUTH_SERVICE,
         transport: Transport.TCP,
